6d27bea9345477d6a1fe099e15bf67ac0744ffbf
shank9 <luizf9@protonmail.com>
2020-04-17 09:04:41 -0300

    lixo

--- bot.js
+++ bot.js
@@ -8,6 +8,13 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
+express()
+.use(express.static(path.join(__dirname, 'public')))
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'ejs')
+.get('/', (req, res) => res.render('pages/index'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 client.on("ready", () => {
   console.log(`Bot foi iniciado, com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores.`); 
   client.user.setPresence({ game: { name: 'comando', type: 1, url: 'discord.gg/rde'} });
